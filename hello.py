4da81a069ec04affc04bd92ec3b1c91028dfbe43
[ ] <36614790+UnZero2126@users.noreply.github.com>
2024-06-12 23:07:38 +0800

    add python files

--- hello.py
+++ hello.py
@@ -1,3 +1,33 @@
 name = input("Name: ")
+#Define lists of names
+names = ["jann", "railey", "thomas", "eddie"]
+#tuple
+coordinates = (100.00, 89.00)
+#set
+s = set()
+#ading elemts on set
+s.add(1)
+s.add(2)
+s.add(3)
+s.add(4)
+s.add(1)
 
+print(f"The set has {len(s)} elements.")
+print(s)
 print(f"Hello, {name}")
+print(names)
+
+names.append(name)
+
+names.sort()
+
+print(names)
+
+n = int(input("Number: "))
+
+if n > 0:
+    print("n is positive")
+elif n < 0:
+    print("n is negative")
+else:
+    print("n is zero")
---
+++ looping.py
@@ -0,0 +1,22 @@
+for i in range(5):
+    print(i)
+
+#looping of names
+names = ["jann", "joe", " raco", "eddie"]
+name1 = "joemomma"
+for name in names:
+    print(name)
+
+for character in name1:
+    print (character)
+
+#defining dictionary
+my_dict = {
+    "jann": "male",
+    "angel": "female",
+    "railey": "male"
+}
+
+my_dict["angel"] = "male"
+
+print(my_dict["jann"])
